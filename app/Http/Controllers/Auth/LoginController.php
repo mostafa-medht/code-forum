d8d8b26356d5a05082f6726fe7d3f76fcbf2894a
mostafamedht98 <mostafamedht98"gmail>
2021-01-04 16:07:40 +0200

    Make GitHub Login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+ 
+use App\User;
+
 
 class LoginController extends Controller
 {
@@ -37,4 +42,42 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        $githubUser = Socialite::driver('github')->user();
+        // dd($githubUser);
+        $user = User::where('provider_id', $githubUser->getId())->first();
+        
+        if (!$user) {
+            // Add User To DataBase 
+            $user= User::create([
+            'email' => $githubUser->getEmail(),
+            'name' => $githubUser->getName(),
+            'avatar' => $githubUser->getAvatar(),
+            'provider_id' => $githubUser->getId()
+            ]);
+        }
+        
+        // Login the User 
+        Auth::login($user, true);
+
+        return redirect($this->redirectTo);
+    }
+    
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 class CreateUsersTable extends Migration
 {
     /**
@@ -16,9 +17,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('avatar')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('provider_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
